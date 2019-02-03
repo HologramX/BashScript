4c7e0469af1aa97fa5ff162b943619fda18f1dc2
HologramX <36265689+HologramX@users.noreply.github.com>
2019-02-03 22:47:16 +0100

    Add files via upload

---
+++ 3dcDestuck.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+EDITOR=vim
+COIN_NAME="3dcoin"
+FOLDER="3dcoin"
+CONFIG_FOLDER="$HOME/.$FOLDER"
+CONFIG_FILE="$COIN_NAME.conf"
+DE="d"
+COIN_DAEMON="$COIN_NAME$DE"
+COIN_CLI="$COIN_NAME-cli"
+COIN_PATH="/usr/local/bin/"
+
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m' 
+PURPLE='\033[0;35m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+STD='\033[0m'
+MAG='\e[1;35m'
+
+ 
+printf "\n"
+printf "${YELLOW}#########################################################################${NC}\n"
+printf "${GREEN}                     $COIN DESTUC by RESYNK  $COIN_NAME                               ${NC}\n"
+printf "${YELLOW}#########################################################################${NC}\n"
+
+$COIN_CLI stop
+sleep 1
+rm -r $CONFIG_FOLDER/chainstate/
+rm -r $CONFIG_FOLDER/blocks/
+$COIN_DAEMON -daemon
+exit
