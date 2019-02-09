2241051304f8baf09228dc47866d787dbe32c34f
HologramX <36265689+HologramX@users.noreply.github.com>
2019-02-09 10:31:32 +0100

    Update 3dcDestuck.sh

--- 3dcDestuck.sh
+++ 3dcDestuck.sh
@@ -38,19 +38,6 @@ rm -f $HOME/.$FOLDER/netfulfilled.dat
 rm -f $HOME/.$FOLDER/governance.dat
 rm -f $HOME/.$FOLDER/debug.log
 rm -f $HOME/.$FOLDER/3dcoind.pid
-echo "addnode=206.189.72.203
-addnode=206.189.41.191
-addnode=165.227.197.115
-addnode=167.99.87.86
-addnode=159.65.201.222
-addnode=159.65.148.226
-addnode=165.227.38.214
-addnode=159.65.167.79
-addnode=159.65.90.101
-addnode=128.199.218.139
-addnode=174.138.3.33
-addnode=159.203.167.75
-addnode=138.68.102.67" >> $HOME/.$FOLDER/$COIN_NAME.conf
 $COIN_DAEMON -daemon
 exit
 
