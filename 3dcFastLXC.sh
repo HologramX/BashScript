2f3ee47f7f36ef814c97f29dc9f4a99f9b07c017
HologramX <36265689+HologramX@users.noreply.github.com>
2019-07-22 17:23:21 +0200

    Update 3dcFastLXC.sh

--- 3dcFastLXC.sh
+++ 3dcFastLXC.sh
@@ -86,7 +86,7 @@ $external_ip_line
 
 rm -v /etc/ssh/ssh_host_* >/dev/null 2>&1
 #dpkg-reconfigure -u openssh-server
-yes |  apt-get remove openssh-server
+#yes |  apt-get remove openssh-server
 cd ~
 
 #echo -e "${GREEN}Downloading and Installing VPS $COIN_NAME Daemon${NC}"
