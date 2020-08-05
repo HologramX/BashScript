a8747c6d30d9b966c38d4608d36e857b0dfdb062
HologramX <36265689+HologramX@users.noreply.github.com>
2020-08-06 00:41:45 +0200

    Add files via upload

--- 3DCPurge.sh
+++ 3DCPurge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 EDITOR=vim
-choice=""
 LOCALE="LANG=en_US.UTF-8
 LC_ALL=en_US.UTF-8
+LANGUAGE=en:it:en"
 
 
 BLUE="\033[0;34m"
@@ -15,29 +15,36 @@ NC='\033[0m'
 MAG='\e[1;35m'
 STD='\033[0m'
 
+
 kill -9 $(pgrep 3dcoind)
-echo ""
+
 echo  -e "${GREEN} Firewall setup.....              ${STD}"
  ufw disable
- ufw delete allow 6695/tcp  
+ ufw delete allow 6695  
  ufw logging on 
 yes |  ufw enable 
+
 echo ""
-echo  -e "${GREEN} 3DCoin File remove...               ${STD}"
+echo  "${GREEN} 3DCoin File remove...               ${STD}"
+
 rm /root/cron*
 rm -rf /usr/local/bin/Masternode
 rm /usr/local/bin/3dc*
-rm -r /root/.3dcoin/
-rm 3dcoin*
+rm -r /root/3dcoin
+rm jail.local
 
-echo  -e "${GREEN} Crontab Purge.....              ${STD}"
+echo ""
+echo  "${GREEN} Crontab Purge.....              ${STD}"
+
 cd ~
 crontab -l > cron
+h=$(( RANDOM % 23 + 1 ));
 crontab -r
 sed -i '/3dcoind/d' ./cron
 sed -i '/Masternode/d' ./cron
 crontab /root/cron
-echo  -e "${GREEN} 3DCoin Node PURGED successfully .....               ${STD}"
+
+echo  -e "${GREEN} 3DCoin core PURGED successfully .....               ${STD}"
 echo ""
 
 rm 3DC*.sh* > /dev/null 2>&1 
